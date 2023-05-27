58523c4e5855281d6bf60a0435024d49edf1a3de
Zeltoria <123985221+Zeltoria@users.noreply.github.com>
2023-05-27 07:07:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {
 const path = require('path')
 
 function start() {
-   let args = [path.join(__dirname, 'alpha.js'), ...process.argv.slice(2)]
+   let args = [path.join(__dirname, 'herta.js'), ...process.argv.slice(2)]
    console.log([process.argv[0], ...args].join('\n'))
    let p = spawn(process.argv[0], args, {
          stdio: ['inherit', 'inherit', 'inherit', 'ipc']
